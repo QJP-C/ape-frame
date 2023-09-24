ae72c3829fc00257e8e7e14681e287ebab170c00
qjp <915950092@qq.com>
2023-09-24 20:35:36 +0800

    给功能做开关

--- ape-common/ape-common-mybatisplus/src/main/java/com/qjp/config/MyBatisConfiguration.java
+++ ape-common/ape-common-mybatisplus/src/main/java/com/qjp/config/MyBatisConfiguration.java
@@ -1,6 +1,7 @@
 package com.qjp.config;
 
 import com.qjp.inteceptor.SqlBeautyInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +11,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MyBatisConfiguration {
     @Bean
+    //给日志优化功能做一个开关            配置文件中的key           值为”true“才生效                       默认值 true 开启
+    @ConditionalOnProperty(name = {"sql.beauty.show"},havingValue = "true",matchIfMissing = true)
     public SqlBeautyInterceptor sqlBeautyInterceptor(){
         return new SqlBeautyInterceptor();
     }
